e67b3860536eae22e9168d3b9de8f3c88a0a245c
mariajalmeida <mari9d91@stud.kea.dk>
2021-02-02 18:46:01 +0100

    modal semi-working

--- script.js
+++ script.js
@@ -9,22 +9,43 @@ function getActors() {
 function handleActors(actors) {
     const myData = actors;
     console.log("mydatay", myData)
-    myData.forEach(divideActors);
+    myData.forEach(separateData);
+}
+
+function separateData(data) {
+    divideActors(data);
+    openModal(data);
 }
 
 function divideActors(singleActor) {
-    console.log(singleActor);
 
-    const template = document.querySelector("template").content;
+    const template = document.querySelector("#actor").content;
     const clone = template.cloneNode(true);
 
     const actorName = clone.querySelector("h2");
     actorName.textContent = singleActor.fullname;
-    const actorsMovie = clone.querySelector("h3");
-    actorsMovie.textContent = singleActor.movie;
 
-    const img = singleActor.img;
+    clone.querySelector("button").addEventListener("click", openModal);
+
+    document.querySelector("main").appendChild(clone);
+}
+
+function openModal(e) {
+    const template = document.querySelector("#modal").content;
+    const clone = template.cloneNode(true);
+
+    const actorsMovie = clone.querySelector("h3");
+    actorsMovie.textContent = e.movie;
+    const img = e.img;
     clone.querySelector("img").src = img;
 
     document.querySelector("main").appendChild(clone);
+
+
+    const modal = document.querySelector(".modal-background");
+    modal.classList.remove("hide");
+    const closeBtn = document.querySelector("span");
+    closeBtn.addEventListener("click", () => {
+        modal.classList.add("hide");
+    } )
 }
